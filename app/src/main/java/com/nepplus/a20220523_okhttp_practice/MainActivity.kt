cd70d1689669b49bf6263b9d6b857e6d81b1aa69
smlee <larc21c@gmail.com>
2022-05-23 15:37:17 +0900

    로그인 기능 작업 완료

--- app/src/main/java/com/nepplus/a20220523_okhttp_practice/MainActivity.kt
+++ app/src/main/java/com/nepplus/a20220523_okhttp_practice/MainActivity.kt
@@ -2,9 +2,11 @@ package com.nepplus.a20220523_okhttp_practice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.nepplus.a20220523_okhttp_practice.databinding.ActivityMainBinding
 import com.nepplus.a20220523_okhttp_practice.utils.ServerUtil
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,7 +24,28 @@ class MainActivity : AppCompatActivity() {
         val inputEmail = binding.emailEdt.text.toString()
         val inputPw = binding.passwordEdt.text.toString()
 
-        ServerUtil.postRequestLogin(inputEmail, inputPw)
+        ServerUtil.postRequestLogin(inputEmail, inputPw, object  : ServerUtil.Companion.JsonResponseHandler{
+            override fun onResponse(jsonObj: JSONObject) {
+
+//                UI입장에서, 로그인 결과를 받아서 처리할 코드
+//                서버에 다녀오고 나서 실행: OkHttp 라이브러리가 자동으로 백그라운드에서 돌도록 만들어둔 코드
+                val code = jsonObj.getInt("code")
+
+                if (code == 200){
+
+                    runOnUiThread {
+                        Toast.makeText(this@MainActivity, "로그인 성공", Toast.LENGTH_SHORT).show()
+                    }
+
+                }
+                else{
+                    val message = jsonObj.getString("message")
+                    runOnUiThread {
+                        Toast.makeText(this@MainActivity,message,Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        })
 
         ServerUtil.BASE_URL
 
--- app/src/main/java/com/nepplus/a20220523_okhttp_practice/utils/ServerUtil.kt
+++ app/src/main/java/com/nepplus/a20220523_okhttp_practice/utils/ServerUtil.kt
@@ -7,18 +7,27 @@ import java.io.IOException
 
 class ServerUtil {
 
+//    서버유틸로 돌아온 응답을 => 엑티비티에서 처리하도록, 일처리 넘기기
+//    나에게 생긴 일을 > 다른 클래스에게 처리 요청 : interface 활용
+
+
+
 //    서버에 Requeset 날리는 역할
 //    함수를 만들려고 하는데, 어떤 객체가 실행해도 결과만 잘 나오면 그만임 함수
 //    코틀린에서 JAVA의 static에 해당하는 개념? Companion object {  } 에 만들자
 
 
-    companion object{
+    companion object {
 
-//        서버 컴퓨터 주소만 변수로 저장 (관리 일원화)
+        interface JsonResponseHandler {
+            fun onResponse(jsonObj: JSONObject)
+        }
+
+        //        서버 컴퓨터 주소만 변수로 저장 (관리 일원화)
         val BASE_URL = "http://54.180.52.26"
 
-//        로그인 기능 호출 함수
-        fun postRequestLogin (email : String, pw : String){
+        //        로그인 기능 호출 함수
+        fun postRequestLogin(email: String, pw: String, handler: JsonResponseHandler?) {
 
 //            Request 제작 -> 실제 호출 -> 서버의 응답을 화면에 전달
 //            제작1) 어느 주소 (url)로 접근 할지 ? => 서버주소 + 기능 주소
@@ -27,7 +36,7 @@ class ServerUtil {
 //            제작 2) 파라미터 담아두기 => 어떤 이름표 / 어느 공간에
             val formData = FormBody.Builder()
                 .add("email", email)
-                .add("password",pw)
+                .add("password", pw)
                 .build()
 
 //           제작 3) 모든 Request 정보를 종합한 객체 생성 ( 어떤주소로 + 어떤 메쏘드로 + 어떤 파라미터로 )
@@ -45,45 +54,47 @@ class ServerUtil {
 //          호출을 했으면 , 서버가 수행한 결과 (Response)를 받아서 처리
 //          =>서버에 다녀와서 할일을 등록 :  enqueue ( Callback)
 
-        client.newCall(request).enqueue(object : Callback{
-            override fun onFailure(call: Call, e: IOException) {
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
 
-            }
+                }
 
-            override fun onResponse(call: Call, response: Response) {
+                //
+                override fun onResponse(call: Call, response: Response) {
 
 
-                val bodyString = response.body!!.string()
-                val jsonObj = JSONObject(bodyString)    //.string()1회용 딱 한번만 사용할수있다.
+                    val bodyString = response.body!!.string()
+                    val jsonObj = JSONObject(bodyString)    //.string()1회용 딱 한번만 사용할수있다.
 
-                Log.d("서버 테스트",jsonObj.toString())
+                    Log.d("서버 테스트", jsonObj.toString())
 
 //                연습  : 로그인 성공 / 실패에 따른 로그 추출
 //                "code" 이름표의 Int를 추출, 그값을 조건문 확인
 
-                val code = jsonObj.getInt("code")
+//                val code = jsonObj.getInt("code")
+//
+//                if(code == 200){
+////                    로그인 시도 성공
+//                    Log.d("로그인 시도","성공")
+//                    val dataObj = jsonObj.getJSONObject("data")
+//                    val userObj = dataObj.getJSONObject("user")
+//                    val nickname = userObj.getString("nick_name")
+//
+//                    Log.d("로그인 성공","닉네임 : $nickname" )
+//                }
+//                else{
+//                    Log.d("로그인 시도","실패")
+//                    val message = jsonObj.getString("message")
+//                    Log.d("실패 사유",message)
+//                }
+//                 실제 : handler 변수에 jsonObj를 가지고 화면에서 어떻게 처리할지 계획이 들어있다.
+//                    계획이 있을때만
+                    handler?.onResponse(jsonObj)
 
-                if(code == 200){
-//                    로그인 시도 성공
-                    Log.d("로그인 시도","성공")
-                    val dataObj = jsonObj.getJSONObject("data")
-                    val userObj = dataObj.getJSONObject("user")
-                    val nickname = userObj.getString("nick_name")
-
-                    Log.d("로그인 성공","닉네임 : $nickname" )
                 }
-                else{
-                    Log.d("로그인 시도","실패")
-                    val message = jsonObj.getString("message")
-                    Log.d("실패 사유",message)
-                }
-
-            }
-        })
-
+            })
 
         }
 
     }
-
 }
